ddb5ea7887f20efa322e21153fe7991f3fe1461f
Jeremy Winget <contact@jrwinget.com>
2025-04-13 19:55:56 -0500

    Enhances documentation and functionality
    - Adds new imports for improved functionality, including cli and jsonlite.
    - Incorporates DiagrammeR into suggests for visualization capabilities.
    - Improves R vignette by capitalizing titles and enforcing dplyr's select usage.
    - Removes an embedded diagram and adjusts code to disable evaluation in non-essential chunks.
    - Optimizes select statements for clarity and consistency.
    - Refines documentation for more structured and user-centric dashboard design.

--- R/bidux-package.R
+++ R/bidux-package.R
@@ -3,6 +3,7 @@
 
 ## usethis namespace: start
 #' @importFrom dplyr filter
+#' @importFrom dplyr select
 #' @importFrom stringdist stringdistmatrix
 #' @importFrom tibble tibble
 ## usethis namespace: end
